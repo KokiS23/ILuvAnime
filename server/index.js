af25accd48829278506368986b156b2e684174f7
unknown <kokishid@buffalo.edu>
2021-02-09 15:57:58 -0500

    View counter

--- server/index.js
+++ server/index.js
@@ -3,21 +3,33 @@ const express = require('express');
 const ejs = require ('ejs');
 const path = require('path');
 
-const clientPath = path.join(__dirname,'../client');
-const staticPath = path.join(clientPath, '/static');
-const viewsPath = path.join(clientPath, '/views');
+const clientPath = path.join(__dirname,'../client/');
+const staticPath = path.join(clientPath, '/static/');
+const viewsPath = path.join(clientPath, '/views/');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', viewsPath);
 
+var x = 0;
+
+const counter = function(req, res, next) {
+  x++;
+  console.log(x);
+  next();
+}
+
+app.use(counter);
+
+app.use(express.static(staticPath));
+
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/about', function(req, res) {
-  res.render('about');
+app.get('/about', counter, function(req, res) {
+  res.render('about',{count: x});
 });
 
 app.get('/contact', function(req, res) {
@@ -28,7 +40,5 @@ app.get('/gallery', function(req, res) {
   res.render('gallery');
 });
 
-//adds middleware to the program
-app.use(express.static(staticPath));
 
 app.listen(2000);
