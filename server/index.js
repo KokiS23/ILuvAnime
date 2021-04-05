a9563c539cfdef423d293820d8f5472efe2ef2ff
KokiS23 <kshido23@gmail.com>
2021-04-04 20:51:32 -0400

    Tutorial 3 completed!

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ejs = require ('ejs');
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 //Navigation
 const clientPath = path.join(__dirname,'../client/');
@@ -12,6 +13,15 @@ const viewsPath = path.join(clientPath, '/views/');
 const app = express();
 app.use(express.static(staticPath));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+  name: 'anime',
+  secret: 'eachcathad7kittens',
+  saveUninitialized: true,
+  resave: false,
+  cookie: {
+    maxAge: 1000*60*60*24*3,
+  }
+}));
 app.listen(2000);
 
 //Setting Views
@@ -27,26 +37,25 @@ const counter = function(req, res, next) {
 }
 
 //Routes
-var userName = '';
-
 app.get('/index', function(req, res) {
-  res.render('index', {nomen: userName});
+  //console.log(req.session);
+  res.render('index', {data: req.session});
 });
 
-app.get('/about', counter, function(req, res) {
-  res.render('about',{count: x});
+app.get('/about', function(req, res) {
+  res.render('about', {data: req.session});
 });
 
 app.get('/contact', function(req, res) {
-  res.render('contact');
+  res.render('contact', {data: req.session});
 });
 
 app.get('/gallery', function(req, res) {
-  res.render('gallery');
+  res.render('gallery', {data: req.session});
 });
 
 app.post('/welcome', (request, response) => {
-  console.log(request.body.visitorname);
-  userName = request.body.visitorname;
-  response.redirect('/index');
+  //console.log(request.body);
+  request.session.username = request.body.nombre;
+  response.send('SUCCESS');
 });
