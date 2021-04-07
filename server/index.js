fc72e837d71136a050575dee61713f52b52e6e58
KokiS23 <kshido23@gmail.com>
2021-04-06 23:24:54 -0400

    Tutorial 4 In Progress

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const ejs = require ('ejs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const mongoose = require('mongoose');
+const {BlogPost} = require('./models.js')
 
 //Navigation
 const clientPath = path.join(__dirname,'../client/');
@@ -22,6 +24,7 @@ app.use(session({
     maxAge: 1000*60*60*24*3,
   }
 }));
+mongoose.connect('mongodb://localhost:27017/anime', {useNewUrlParser: true});
 app.listen(2000);
 
 //Setting Views
@@ -54,8 +57,35 @@ app.get('/gallery', function(req, res) {
   res.render('gallery', {data: req.session});
 });
 
+app.get('/blog/', async (req, res) => {
+  var posts = await BlogPost.find({}, (error, result) => {
+    if(error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+    console.log(result);
+    res.render('blog', {data: req.session, postset: result});
+  });
+});
+
+app.get('/blog/write/', (req, res) => {
+  res.render('writing', {data: req.session});
+});
+
+app.get('/blog/writepost/', async (req, res) => {
+  console.log(req.body);
+  let newPost = new BlogPost(req.body);
+  await newPost.save();
+  res.redirect('/blog/');
+});
+
 app.post('/welcome', (request, response) => {
   //console.log(request.body);
   request.session.username = request.body.nombre;
   response.send('SUCCESS');
+});
+
+app.post('/writeblogpost', (request, response) => {
+  console.log(request.body);
+  response.redirect('/index');
 });
